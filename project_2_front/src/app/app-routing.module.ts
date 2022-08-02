b7e200dd9bf6a1d791a1a087daf4d6a06767a70a
unknown <fred.kelemen99@gmail.com>
2022-08-02 11:27:12 -0400

    Started making orders component

--- project_2_front/src/app/app-routing.module.ts
+++ project_2_front/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './components/orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "orders", component: OrdersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
