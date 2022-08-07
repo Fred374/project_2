133f04ef465ca1968b4e2a6d673c5ebe4e759eb0
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-06 19:15:27 -0700

    Add all Controller classes, still need to write implementation.

---
+++ project_2_back/src/main/java/com/revature/controllers/OrderItemController.java
@@ -0,0 +1,10 @@
+package com.revature.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/order_item")
+public class OrderItemController {
+
+}
