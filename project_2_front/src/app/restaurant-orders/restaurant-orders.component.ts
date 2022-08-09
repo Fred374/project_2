78817861568449bcd971f43cea98fdc498c4d51c
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-09 14:21:25 -0700

    Add container div in app.component.html. Add table in restaurant-orders component

--- project_2_front/src/app/restaurant-orders/restaurant-orders.component.ts
+++ project_2_front/src/app/restaurant-orders/restaurant-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../services/orders.service';
 
 @Component({
   selector: 'app-restaurant-orders',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantOrdersComponent implements OnInit {
 
-  constructor() { }
+  testArr = ["One", "Two", "Three", "Four", "Five"]
+
+  constructor(private orderService:OrdersService) { }
 
   ngOnInit(): void {
   }
