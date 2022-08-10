37a74b524dd5520a129030e6e6158b46def199c3
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-10 10:57:35 -0700

    Add implementation for populationg orders table. Minor refactor in Order model

--- project_2_front/src/app/models/order.ts
+++ project_2_front/src/app/models/order.ts
@@ -1,6 +1,7 @@
 import { OrderStatus } from "./order-status";
 import { OrderItem } from "./order-item";
 import { User } from "./user";
+import { Timestamp } from "rxjs";
 
 export class Order {
     constructor(
@@ -9,7 +10,8 @@ export class Order {
         public orderItems: OrderItem[],
         public orderId?: number,
         public userId?: User,
-        public orderStatusId?: OrderStatus
+        public orderStatusId?: OrderStatus,
+        public orderTimestamp?: Date,
         // Probably need ot add list of OrderItems
     ) { }
 }
