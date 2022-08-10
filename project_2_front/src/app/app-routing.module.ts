e9bba55c455d8118e62ce97dee2580c5db7d2aa4
IsraelAfangideh <israelafangideh@gmail.com>
2022-08-10 14:14:45 -0500

    Routed from choose restaurant to order component, reorganized components into the components folder

--- project_2_front/src/app/app-routing.module.ts
+++ project_2_front/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ChooserestaurantComponent } from './components/orders/chooserestaurant/chooserestaurant.component';
+import { ChooserestaurantComponent } from './components/chooserestaurant/chooserestaurant.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { MoodforwhatComponent } from './components/moodforwhat/moodforwhat.component';
