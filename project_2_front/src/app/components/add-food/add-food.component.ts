61776b150d28e1aa03962fa384940979b3239090
unknown <fred.kelemen99@gmail.com>
2022-08-11 12:02:42 -0400

    Further work on the add menu items

--- project_2_front/src/app/components/add-food/add-food.component.ts
+++ project_2_front/src/app/components/add-food/add-food.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FoodItem } from 'src/app/models/food-item';
 import { OrdersService } from 'src/app/services/orders.service';
 
@@ -9,22 +10,39 @@ import { OrdersService } from 'src/app/services/orders.service';
 })
 export class AddFoodComponent implements OnInit {
 
-  public foodItem : FoodItem = {
-    foodItemId : 0,
-    foodItemName : "",
-    foodItemCost : 0,
-    restaurantIdFk : 4966752
-  }
+  resId = 4966752;
 
-  constructor(private os: OrdersService) { }
+  public foodItemArr: FoodItem[] = [];
+
+  public foodItem: FoodItem = {
+    foodItemId: 0,
+    foodItemName: "",
+    foodItemCost: 0,
+    restaurantIdFk: this.resId
+  };
+
+  constructor(private os: OrdersService, private router: Router) { }
+
+  getFoods() {
+    this.os.getFood(this.resId).subscribe(
+      (data: any) => {
+        this.foodItemArr = data.body;
+      }
+    );
+  }
 
   addFood() {
     console.log(this.foodItem);
     this.os.addFood(this.foodItem).subscribe(
       (data: any) => {
-        this.foodItem = data;
-        console.log(this.foodItem);
         alert("Menu Item has been added");
+        this.foodItem = {
+          foodItemId: 0,
+          foodItemName: "",
+          foodItemCost: 0,
+          restaurantIdFk: this.resId
+        };
+        this.getFoods();
       }, () => {
         alert("An error occured.");
       }
@@ -32,6 +50,7 @@ export class AddFoodComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.getFoods();
   }
 
 }
