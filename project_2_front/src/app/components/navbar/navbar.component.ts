711068bac35ca1eb44bc3ff433c1fb4efc0b1d5c
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-12 12:39:09 -0700

    UI improvements in restaruant orders component

--- project_2_front/src/app/components/navbar/navbar.component.ts
+++ project_2_front/src/app/components/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { User } from 'src/app/models/user';
 })
 export class NavbarComponent implements OnInit {
 
-  public user: User = new User();
+  public user: User = JSON.parse(localStorage.getItem("currentUser") || "");
 
   constructor() { }
 
@@ -16,7 +16,7 @@ export class NavbarComponent implements OnInit {
   }
 
   ngDoCheck(): void {
-    // this.user = JSON.parse(localStorage.getItem("currentUser") || "");
+    this.user = JSON.parse(localStorage.getItem("currentUser") || "");
   }
 
 }
