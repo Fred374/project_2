56899825b78ac2d4911dadeccb4affc5ecf22722
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-13 16:51:27 -0700

    Add footer. Improvements to navbar and minor UI improvements

--- project_2_front/src/app/components/navbar/navbar.component.ts
+++ project_2_front/src/app/components/navbar/navbar.component.ts
@@ -4,30 +4,45 @@ import { User } from 'src/app/models/user';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-
-  public user: User = JSON.parse(localStorage.getItem("currentUser") || "{}");
+  // User Info
+  public user: User = JSON.parse(localStorage.getItem('currentUser') || '{}');
   public userRoleId: number = 0;
-  public userFirstName: string = ""
+  public userFirstName: string = '';
 
-  constructor() { }
+  // Home Page for user
+  public homePage: string = '';
 
-  ngOnInit(): void {
+  constructor() {}
 
-  }
+  ngOnInit(): void {}
 
   ngDoCheck(): void {
-    this.user = JSON.parse(localStorage.getItem("currentUser") || "{}");
+    this.user = JSON.parse(localStorage.getItem('currentUser') || '{}');
 
     this.userRoleId = this.user.userRoleId?.userRoleId!;
     this.userFirstName = this.user.userFirstName!;
+
+    this.setUserHomePage();
+  }
+
+  setUserHomePage() {
+    // Setting home page depending on userRoleId
+    if (this.userRoleId == 1) {
+      this.homePage = '/mood';
+    } else if (this.userRoleId == 2) {
+      this.homePage = '/driver/orders';
+    } else if (this.userRoleId == 3) {
+      this.homePage = '/restaurant/orders';
+    } else {
+      this.homePage = '/asdafasfs';
+    }
   }
 
   logoutPressed() {
     localStorage.removeItem('currentUser');
     this.userRoleId = 0;
   }
-
 }
