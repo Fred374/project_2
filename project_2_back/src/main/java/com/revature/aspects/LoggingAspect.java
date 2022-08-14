69d28a1888323493b63c56c5946a2196efe84d84
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-14 16:28:25 -0700

    Implement logging

--- project_2_back/src/main/java/com/revature/aspects/LoggingAspect.java
+++ project_2_back/src/main/java/com/revature/aspects/LoggingAspect.java
@@ -1,40 +1,33 @@
 package com.revature.aspects;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component 
 @Aspect
 public class LoggingAspect {
 	
-		private static Logger log = LogManager.getLogger();
-		
-		
-		@Before("within(com.revature.controllers.*)")
-		public void logModelMethods(JoinPoint jp) {
-			
-			
-			log.info(jp.getTarget() + " invoked " + jp.getSignature());
-			
-		}
-		
-		@AfterReturning(pointcut="execution(String fight(..))", returning="returnedObject")
-		public void logSuccessfulFight(JoinPoint jp, Object returnedObject) {
-			
-			log.info(jp.getTarget() + " invoked " + jp.getSignature() + " and returned " + returnedObject);
-			
-			
-			
-		}
-		
-		
+	Logger log = LoggerFactory.getLogger(LoggingAspect.class);
 	
+	
+	@Before("within(com.revature.controllers.*)")
+	public void logModelMethods(JoinPoint jp) {
+		
+		log.info(jp.getTarget() + " invoked " + jp.getSignature());
 		
 	}
+	
+//	@AfterReturning(pointcut="within(com.revature.controllers.*)", returning="returnedObject")
+//	public void logSuccessfulFight(JoinPoint jp, Object returnedObject) {
+//		
+//		log.info(jp.getTarget() + " invoked " + jp.getSignature() + " and returned " + returnedObject);	
+//		
+//	}
+}
 
 
