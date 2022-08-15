5b0dfa590595bb1fbabdb7d13fcd63406ad6db30
Vahe Aslanyan <vaheaslanyan@macbook1hunnid.local>
2022-08-14 18:59:01 -0700

    Improvements in logging. Start Junit

--- project_2_back/src/test/java/com/revature/Project2BackApplicationTests.java
+++ project_2_back/src/test/java/com/revature/Project2BackApplicationTests.java
@@ -1,13 +1,55 @@
 package com.revature;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.controllers.UserController;
+import com.revature.daos.UserDAO;
+import com.revature.models.User;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Project2BackApplicationTests {
 
+	@Autowired
+	@MockBean
+	private UserController userController;
+	
+	@MockBean
+	private UserDAO userDAO;
+	
+	
+	
 	@Test
-	void contextLoads() {
+	public void getUsersTest() {
+
+		ResponseEntity r = new ResponseEntity(HttpStatus.ACCEPTED);
+				
+		when(userController.getAllUsers()).thenReturn(r);
+	
+		assertEquals(HttpStatus.ACCEPTED, userController.getAllUsers().getStatusCode());
 	}
 
+//	new User("user1", "pass1", "first", "last", "user1@email.com"), new User("user2", "pass2", "first", "last", "user2@email.com"), new User("user3", "pass3", "first", "last", "user3@email.com"))
+//	this.userId = userId;
+//	this.userUsername = userUsername;
+//	this.userPassword = userPassword;
+//	this.userFirstName = userFirstName;
+//	this.userLastName = userLastName;
+//	this.userEmail = userEmail;
 }
