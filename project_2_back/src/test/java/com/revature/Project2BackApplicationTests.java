54aef58dfa83c85a5ea82db115be507d77dcbd43
Ahmad-K <ahmad093@revature.net>
2022-08-15 02:33:46 -0400

    Merge branch 'Ahmad-4' of https://github.com/Fred374/project_2.git into Ahmad-5
    # Conflicts:
    #	project_2_front/src/app/components/registration/registration.component.ts

--- project_2_back/src/test/java/com/revature/Project2BackApplicationTests.java
+++ project_2_back/src/test/java/com/revature/Project2BackApplicationTests.java
@@ -1,13 +1,68 @@
 package com.revature;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.revature.controllers.OrderController;
+import com.revature.controllers.UserController;
+import com.revature.daos.UserDAO;
+import com.revature.daos.UserRoleDAO;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class Project2BackApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	@MockBean
+	static private UserController userController;
+	
+	@Autowired
+	@MockBean
+	static private OrderController orderController;
+	
+	@MockBean
+	static private UserDAO userDAO;
+	
+	@MockBean
+	static private UserRoleDAO userRoleDAO;
+	
+	@BeforeAll
+	static public void setup() {
+//		userController = Mockito.spy(new UserController(userDAO, userRoleDAO));
 	}
+	
+	
+	@Test
+	public void getUsersTest() {
 
+		ResponseEntity r = new ResponseEntity(HttpStatus.ACCEPTED);
+				
+		when(userController.getAllUsers()).thenReturn(r);
+	
+		assertEquals(HttpStatus.ACCEPTED, userController.getAllUsers().getStatusCode());
+	}
+	
+//	@Test
+//	public void getUserById() {
+//		
+//		Order mockOrder = Mockito.mock(Order.class);
+//		
+//		OngoingStubbing<Order> mockReturnedOrder = when(orderController.findOrderById(1).getBody()).thenReturn(mockOrder);
+//		
+//		assertNotNull(mockReturnedOrder);
+//		
+//	}
+	
+	
 }
